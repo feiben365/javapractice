b4e5bd3abe559407da957323343cfa9bf5562cbc
feiben <feiben365@gmail.com>
2026-01-07 18:31:08 -0500

    FullName name changed and coding modified.

--- ch08p/ch08p/src/Fullname.java
+++ ch08p/ch08p/src/Fullname.java
@@ -1,4 +1,4 @@
-public class Fullname
+public class FullName
 {
     private String lastName;
     private String firstName;
@@ -19,16 +19,40 @@ public class Fullname
         middleName = str;
     }
 
-    public int getLength()
+    /*public int getLength()
     {
         return lastName.length() +
                 firstName.length() + middleName.length();
     }
-
+    */
     public String toString()
     {
         return firstName + " " + middleName + " "
                 + lastName;
     }
+
+    public int getLength()
+    {
+        int len = 0;
+
+        if (lastName != null)
+            len +=lastName.length();
+        if (firstName != null)
+            len +=firstName.length();
+        if (middleName != null)
+            len +=middleName.length();
+
+        return len;ß
+
+
+    }
+
+   /* public FullName()
+    {
+        lastName = "";
+        firstName = "";
+        middleName = "";
+    }
+   */
 
 }
