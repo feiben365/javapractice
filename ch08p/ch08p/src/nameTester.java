c3369e6c326b87cfcafba9d8301cead16ad2336e
feiben <feiben365@gmail.com>
2026-01-07 18:27:26 -0500

    NameTester completed

---
+++ ch08p/ch08p/src/nameTester.java
@@ -0,0 +1,13 @@
+public class nameTester
+{
+    public static void main(String[] args)
+    {
+        int len;
+
+        FullName name = new FullName();
+
+        len = name.getLength();
+    }
+
+  
+}
