d443f539fe6378a899c668b0274b5089d8d6add5
feiben <feiben365@gmail.com>
2026-01-08 11:32:29 -0500

    Course practice done

---
+++ ch08p/ch08p/src/CourseP1/Course.java
@@ -0,0 +1,38 @@
+package CourseP1;
+
+public class Course
+{
+    private String courseName;
+    private Instructor instructor;
+    private TextBook textBook;
+
+    public Course(String name, Instructor instr, TextBook text)
+    {
+        courseName = name;
+        instructor = instr;
+        textBook = text;
+    }
+
+    public String getName()
+    {
+        return courseName;
+    }
+
+    public Instructor  getInstructor()
+    {
+        return new Instructor(instructor);
+    }
+
+    public TextBook getTextBook()
+    {
+        return new TextBook(textBook);
+    }
+
+    public String toString()
+    {
+        String str = "Course name: " + courseName +
+                "\nInstructor Information:\n" + instructor
+                + "\nTextBook Information:\n" + textBook;
+        return str;
+    }
+}
