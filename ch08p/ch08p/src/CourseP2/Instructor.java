3b071644f76dd65b58490d6bfb520d927094d860
feiben <feiben365@gmail.com>
2026-01-09 11:04:31 -0500

    Course practice2 done

---
+++ ch08p/ch08p/src/CourseP2/Instructor.java
@@ -0,0 +1,38 @@
+package CourseP2;
+
+public class Instructor
+{
+    private String lastName;
+    private String firstName;
+    private String officeNumber;
+
+    public Instructor(String lname, String fname, String office)
+    {
+        lastName = lname;
+        firstName = fname;
+        officeNumber = office;
+    }
+
+    public Instructor(Instructor object2)
+    {
+        lastName = object2.lastName;
+        firstName = object2.firstName;
+        officeNumber = object2.officeNumber;
+    }
+
+    public void set(String lname, String fname, String office)
+    {
+        lastName = lname;
+        firstName = fname;
+        officeNumber = office;
+    }
+
+    public String toString()
+    {
+        String str ="Last Name: " + lastName +
+            "\nFirst Name: " + firstName +
+            "\nOffice Number: " + officeNumber;
+
+        return str;
+    }
+}
