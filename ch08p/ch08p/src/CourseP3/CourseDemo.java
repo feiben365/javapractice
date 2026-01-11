0fda88593912044339a3f1f3237b26b5629df9ee
feiben <feiben365@gmail.com>
2026-01-10 22:19:56 -0500

    P3 4 files done

--- ch08p/ch08p/src/CourseP3/CourseDemo.java
+++ ch08p/ch08p/src/CourseP3/CourseDemo.java
@@ -1,4 +1,13 @@
 package CourseP3;
 
-public class CourseDemo {
+public class CourseDemo
+{
+    public static void main(String[] args)
+    {
+        Instructor myInstructor = new Instructor("John Smith", "John", "Smith");
+        TextBook myTextBook = new TextBook("Java Programming", "Java", "Programming");
+        Course myCourse = new Course("Java Programming", myInstructor, myTextBook);
+
+        System.out.println(myCourse);
+    }
 }
