5dd98ea74d40cee1ee964f016c6f616ed3842033
feiben <feiben365@gmail.com>
2026-01-12 18:03:00 -0500

    SportsCarDemo added.

--- ch08p/ch08p/src/CarType.java
+++ ch08p/ch08p/src/CarType.java
@@ -1,2 +1,1 @@
-
 enum CarType {PORSCHE, FERRARI, JAGUAR}
--- ch08p/ch08p/src/SportsCar.java
+++ ch08p/ch08p/src/SportsCar.java
@@ -2,5 +2,34 @@ public class SportsCar
 {
     private CarType make;
     private CarColor color;
+    private double price;
+
+    public SportsCar(CarType amake, CarColor aColor, double aPrice)
+    {
+        make = amake;
+        color = aColor;
+        price = aPrice;
+    }
+
+    public CarType getMake()
+    {
+        return make;
+    }
+
+    public CarColor getColor()
+    {
+        return color;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public String toString(){
+        String str = String.format("Make: %s\nColor: %s\nPrice: $%, .2f",
+                make, color, price);
+        return str;
+    }
 
 }
