47225413344ff93d377738acce6ef241988dccad
feiben <feiben365@gmail.com>
2026-01-11 21:54:01 -0500

    P4 Course  done

---
+++ ch08p/ch08p/src/CourseP4/Course.java
@@ -0,0 +1,39 @@
+package CourseP4;
+
+public class Course
+{
+    private String courseName;
+    private Instructor instructor;
+    private TextBook textBook;
+
+    public Course(String name, Instructor instr, TextBook text)
+    {
+        courseName = name;
+        instructor = instr;
+        textBook = text;
+    }
+
+    public String getName()
+    {
+        return courseName;
+    }
+
+    public Instructor getInstructor()
+    {
+        return new Instructor(instructor);
+    }
+
+    public TextBook getTextBook()
+    {
+        return new TextBook(textBook);
+    }
+
+    public String toString()
+    {
+        String str = "Course name: " + courseName +
+                "\nInstructor Information:\n" + instructor
+                + "\nTextBook:\n" + textBook;
+        
+        return str;
+    }
+}
