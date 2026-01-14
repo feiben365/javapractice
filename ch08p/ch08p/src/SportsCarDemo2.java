92859f30c55dad75039698076a70bf4add8d3970
feiben <feiben365@gmail.com>
2026-01-13 21:06:57 -0500

    sportsDemo2

---
+++ ch08p/ch08p/src/SportsCarDemo2.java
@@ -0,0 +1,23 @@
+public class SportsCarDemo2
+{
+    public static void main(String[] args)
+    {
+        SportsCar yourNewCar = new SportsCar(CarType.PORSCHE, CarColor.RED, 100000);
+
+        switch (yourNewCar.getMake())
+        {
+            case PORSCHE:
+                System.out.println("Your car was made in Germany.");
+                break;
+
+            case FERRARI:
+                System.out.println("Your car was made in Italy.");
+                break;
+            case JAGUAR:
+                System.out.println("Your car was made in England.");
+                break;
+                default:
+                    System.out.println("I'm not sure where that car was made.");
+        }
+    }
+}
