ba21011ad3fcf1268aca1085d31444d506cffd2d
feiben <feiben365@gmail.com>
2026-01-17 22:03:35 -0500

    P5 CourseDemo done

--- ch08p/ch08p/src/CourseP5/CouseDemo.java
+++ ch08p/ch08p/src/CourseP5/CouseDemo.java
@@ -2,5 +2,13 @@ package CourseP5;
 
 public class CouseDemo
 {
+    public static void main(String[] args)
+    {
+        Instructor myInstructor = new Instructor("Kramer", "Shawn", "RH3010");
+        TextBook myTextBok = new TextBook("Starting Out with Java", "Gaddis", "Pearson");
+        Course myCourse = new Course("Intro to Java", myInstructor, myTextBok);
 
+
+        System.out.println(myCourse);
+    }
 }
