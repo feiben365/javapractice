b98f4cba0b6dabdfd874e09bef6d3ad749a55c73
feiben <feiben365@gmail.com>
2026-01-21 21:13:24 -0500

    p6 course done

--- ch08p/ch08p/src/CourseP6/Course.java
+++ ch08p/ch08p/src/CourseP6/Course.java
@@ -1,4 +1,38 @@
 package CourseP6;
 
-public class Course {
+public class Course
+{
+    private String courseName;
+    private Instructor instructor;
+    private TextBook textBook;
+
+    public Course(String name, Instructor instr, TextBook text)
+    {
+        courseName = name;
+        instructor = instr;
+        textBook = text;
+    }
+
+    public String getName()
+    {
+        return courseName;
+    }
+
+    public Instructor getInstructor()
+    {
+        return new Instructor(instructor);
+    }
+
+    public TextBook getTextBook()
+    {
+        return new TextBook(textBook);
+    }
+
+    public String toString()
+    {
+        String str = "Course name: " + courseName +
+            "\nInstructor Information:\n" + instructor
+            + "\nTextBook Information:\n" + textBook;
+        return str;
+    }
 }
