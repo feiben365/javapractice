360e169bf732914e547f5c1a5050bc426bc3c310
feiben <feiben365@gmail.com>
2026-01-22 22:46:28 -0500

    p6 CourseDemo done

--- ch08p/ch08p/src/CourseP6/CourseDemo.java
+++ ch08p/ch08p/src/CourseP6/CourseDemo.java
@@ -1,4 +1,14 @@
 package CourseP6;
 
-public class CourseDemo {
+public class CourseDemo
+{
+    public static void main(String[] args)
+    {
+        Instructor myInstructor = new Instructor("Kramer", "Shawn", "RH3010");
+        TextBook myTextBook = new TextBook("Starting Out", "Gaddis", "Pearson");
+        Course myCourse = new Course("Intro to Java", myInstructor, myTextBook);
+
+        System.out.println(myCourse);
+    }
+
 }
