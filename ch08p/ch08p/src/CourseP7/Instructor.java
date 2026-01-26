bf6aa6a77bafeeb69734684284d0895fee8f65cd
feiben <feiben365@gmail.com>
2026-01-26 18:54:32 -0500

    P8 Instructor done

--- ch08p/ch08p/src/CourseP7/Instructor.java
+++ ch08p/ch08p/src/CourseP7/Instructor.java
@@ -1,15 +1,35 @@
 package CourseP7;
 
-public class Instructor
-{
+public class Instructor {
     private String lastName;
     private String firstName;
     private String officeNumber;
 
-    public Instructor(String lname, String fname, String office)
-    {
+    public Instructor(String lname, String fname, String office) {
         lastName = lname;
         firstName = fname;
         officeNumber = office;
     }
+
+    public Instructor(Instructor object2) {
+        lastName = object2.lastName;
+        firstName = object2.firstName;
+        officeNumber = object2.officeNumber;
+    }
+
+    public void set(String lname, String fname, String office)
+    {
+     lastName = lname;
+     firstName = fname;
+     officeNumber = office;
+    }
+
+    public String toString()
+    {
+        String str = "Last name: " + lastName +
+                "\nFirst name: " + firstName
+                + "\nOffice number: " + officeNumber;
+        return str;
+    }
+
 }
