9be6897c9225229cf1e08a3e046dc5aab65e6fd7
feiben <feiben365@gmail.com>
2026-01-27 21:34:31 -0500

    P7 TextBook Done

--- ch08p/ch08p/src/CourseP7/TextBook.java
+++ ch08p/ch08p/src/CourseP7/TextBook.java
@@ -1,4 +1,40 @@
 package CourseP7;
 
-public class TextBook {
+public class TextBook
+{
+    public String title;
+    public String author;
+    public String publisher;
+
+    public TextBook(String textTitle, String auth, String pub)
+    {
+        title = textTitle;
+        author = auth;
+        publisher = pub;
+    }
+
+    public TextBook(TextBook object2)
+    {
+        title = object2.title;
+        author = object2.author;
+        publisher = object2.publisher;
+    }
+
+    public void set(String textTitle, String auth, String pub)
+    {
+        title = textTitle;
+        author = auth;
+        publisher = pub;
+    }
+
+    public String toString()
+    {
+        String str = "Title: " + title +
+            "\nAuthor: " + author +
+            "\nPublisher: " + publisher;
+
+        return str;
+    }
 }
+
+
